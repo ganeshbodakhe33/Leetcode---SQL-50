5c399c0fda7eb2cd7652c66ad02a1d118aba060f
Ganesh Bodakhe <114085049+ganeshbodakhe33@users.noreply.github.com>
2024-07-26 00:40:10 +0530

    Create 197. Rising Temperature.sql

---
+++ 197. Rising Temperature.sql
@@ -0,0 +1,29 @@
+"""
+Given a Weather table, write a SQL query to find all dates' Ids with higher temperature compared to its previous (yesterday's) dates.
+
++---------+------------------+------------------+
+| Id(INT) | RecordDate(DATE) | Temperature(INT) |
++---------+------------------+------------------+
+|       1 |       2015-01-01 |               10 |
+|       2 |       2015-01-02 |               25 |
+|       3 |       2015-01-03 |               20 |
+|       4 |       2015-01-04 |               30 |
++---------+------------------+------------------+
+For example, return the following Ids for the above Weather table:
+
++----+
+| Id |
++----+
+|  2 |
+|  4 |
++----+
+
+
+Solution:
+Join + Datadiff
+"""
+-- Query :
+select w1.id
+from Weather as w1, Weather as w2
+where w1.Temperature > w2.Temperature
+and datediff(w1.recordDate, w2.recordDate) = 1;
